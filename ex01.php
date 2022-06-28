2b45444568306c41f7641ce4290de68b23485bc0
profcleberfidelles <108277066+profcleberfidelles@users.noreply.github.com>
2022-06-28 09:26:29 -0300

    arquivos da primeira versão do nosso site

---
+++ ex01.php
@@ -0,0 +1,33 @@
+<?php
+/*$Nome = 'Cleber Figueiredo Fidelles';
+$nome = 'Andrieli';
+$numero = 10;
+
+var_dump($Nome);
+echo "<br>";
+var_dump($numero);*/
+/*$numero = rand(0,1);
+
+if($numero == 0){
+    echo "<h2>Cara</h2>";
+}
+else{
+    echo "<h2>Coroa</h2>";*/
+
+/*$numero = pi();
+
+echo "o número pi vale $numero";*/
+
+/*$numero = rand(0,20);
+
+if ($numero%2 ==0){
+    echo "o número $numero é par";
+}else{
+    echo "o número $numero é impar";
+}*/
+
+
+
+
+
+?>
---
+++ verificar.php
@@ -0,0 +1,10 @@
+<?php
+$nome = $_GET['nome'];
+$email = $_GET['email'];
+$mensagem = $_GET['msg'];
+
+echo $nome."<br>";
+echo $email."<br>";
+echo $mensagem;
+
+?>
